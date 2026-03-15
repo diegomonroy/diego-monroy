2797f0da3c607952b20509ecc0097e94faff7b93
Diego Fernando Monroy Muñoz <dmsoftwaresas@gmail.com>
2026-03-15 15:46:01 +0000

    Fixed Portfolio project.

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,7 +16,10 @@ export default defineConfig({
       },
     },
   },
-  output: "static",
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
   build: {
     inlineStylesheets: "auto",
   },
